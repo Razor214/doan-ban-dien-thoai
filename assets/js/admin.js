9c8c5eeeee2a4f36465950d7c6aa73a45a1fa44f
Khai <suytiu97@gmail.com>
2025-10-28 15:40:17 +0700

    merge Quan Ly Gia Ban

---
+++ assets/js/admin.js
@@ -0,0 +1,200 @@
+
+    const modal = document.getElementById('popup');
+    const form = document.getElementById('productadd');
+    const cancelBtn = document.getElementById('cancelBtn');
+    const table = document.querySelector('#Bangsp tbody');
+    const searchInput = document.getElementById('Timkiem')
+    const categoryInput = document.getElementById('category');
+    const nameInput = document.getElementById('name');
+    const costInput = document.getElementById('cost');
+    const profitInput = document.getElementById('profit');
+    const priceInput = document.getElementById('sell');
+
+    let editingRow = null;
+    fetch("../data/manageproduct.json")
+        .then(r => r.json())
+        .then(d => khungbang.innerHTML = d.map(sp => `
+            <tr>
+                <td>${sp.brand}</td>
+                <td>${sp.name}</td>
+                <td>${sp.cost.toLocaleString("vi-VN")}</td>
+                <td>${sp.profit}%</td>
+                <td>${(sp.price*(1+sp.profit/100)).toLocaleString("vi-VN")}</td>
+                <td class="action">
+                    <button class="edit">Sửa</button> 
+                    <button class="delete">Xóa</button>
+                </td>
+            </tr>`
+        ).join("")
+    );
+    table.addEventListener("click", (e) => {
+        if (e.target.classList.contains("edit")) {
+            openModal("edit", e.target);
+        } else if (e.target.classList.contains("delete")) {
+            confirmDelete(e.target);
+        }
+    });
+
+    function openModal(mode, btn) {
+        form.reset();
+        priceInput.value = '';
+        modal.style.display = 'flex'; 
+        editingRow = null;
+
+        if (mode === 'edit' && btn) {
+            const row = btn.closest('tr');
+            categoryInput.value = row.cells[0].innerText;
+            nameInput.value = row.cells[1].innerText;
+            costInput.value = row.cells[2].innerText.replace(/,/g,'');
+            profitInput.value = parseFloat(row.cells[3].innerText);
+            priceInput.value = row.cells[4].innerText.replace(/,/g,'');
+            editingRow = row;
+        }
+    }
+
+    cancelBtn.onclick = () => modal.style.display = 'none';
+
+    
+    costInput.addEventListener('input', updatePrice);
+    profitInput.addEventListener('input', updatePrice);
+    priceInput.addEventListener('input', updateProfit);
+
+    function cleanNumber(value){
+        return parseFloat(value.replace(/[.,\s]/g, ''))||0;
+    }
+    function formatNumber(num){
+        return num.toLocaleString('vi-VN');
+    }
+    function updatePrice() {
+        const cost = cleanNumber(costInput.value);
+        const profit = parseFloat(profitInput.value);
+        if (cost >0  && !isNaN(profit)) {
+            const price = cost + (cost * profit / 100);
+            priceInput.value = formatNumber(Math.round(price));
+        }else{
+            priceInput.value='';
+        }
+    }
+
+    function updateProfit() {
+        const cost = cleanNumber(costInput.value);
+        const price = cleanNumber(priceInput.value);
+        if (cost > 0 && price>0) {
+            const profit = ((price - cost) / cost) * 100;
+            profitInput.value = profit.toFixed(2);
+        }
+    }
+    function formatOnInput(input){
+        let val = cleanNumber(input.value);
+        input.value = val ? formatNumber(val) : '';
+    }
+    
+    form.onsubmit = (e) => {
+        e.preventDefault();
+        const category = categoryInput.value;
+        const name = nameInput.value;
+        const cost = costInput.value;
+        const profit = profitInput.value;
+        const price = priceInput.value;
+
+        if (!category || !name || !cost || !profit) {
+            alert('Vui lòng nhập đầy đủ thông tin!');
+            return;
+        }
+
+        if (editingRow) {
+            editingRow.cells[0].innerText = category;
+            editingRow.cells[1].innerText = name;
+            editingRow.cells[2].innerText = cost;
+            editingRow.cells[3].innerText = profit + '%';
+            editingRow.cells[4].innerText = price;
+        } else {
+            const row = table.insertRow();
+            row.innerHTML = `
+                <td>${category}</td>
+                <td>${name}</td>
+                <td>${cost}</td>
+                <td>${profit}%</td>
+                <td>${price}</td>
+                <td class="action">
+                    <button class="edit" onclick="openModal('edit', this)">Sửa</button>
+                    <button class="delete" onclick="confirmDelete(this)">Xóa</button>
+                </td>
+            `;
+        }
+        modal.style.display = 'none';
+    };
+
+    function searchProduct(){
+        const keyword = searchInput.value.trim().toLowerCase();
+        const rows = table.getElementsByTagName('tr');
+
+        for(let row of rows){
+            const productName = row.cells[1].innerText.toLowerCase();
+            row.style.display = productName.startsWith(keyword) || keyword === '' ? '' : 'none';
+
+        }
+    }
+    searchInput.addEventListener('input', searchProduct);
+    costInput.addEventListener('blur', ()=>{
+        let val = cleanNumber(costInput.value);
+        costInput.value = val ? formatNumber(val) : '';
+    });
+    updatePrice();
+    priceInput.addEventListener('blur', ()=>{
+        let val = cleanNumber(priceInput.value);
+        priceInput.value = val ? formatNumber(val) : '';
+    });
+    updatePrice();
+
+
+    function confirmDelete(btn) {
+        const popup = document.getElementById('xacnhan');
+        popup.style.display = 'flex';
+        const row = btn.closest('tr');
+        document.getElementById('xacnhanxoa').onclick = () => {
+            row.remove();
+            popup.style.display = 'none';
+        };
+        document.getElementById('xacnhankhong').onclick = () => popup.style.display = 'none';
+    }
+
+    function confirmExit() {
+        document.getElementById('xacnhanthoat').style.display = 'flex';
+        document.getElementById('confirmco').onclick = () => {
+            document.getElementById('xacnhanthoat').style.display='none';
+            document.querySelector('.admin6').style.display ='none';
+        }
+        document.getElementById('confirmno').onclick = () => document.getElementById('xacnhanthoat').style.display = 'none';
+    }
+
+    function exportData() {
+        document.getElementById('Xuatfile').style.display = 'flex';
+        document.getElementById('confirm').onclick = () => {
+            alert('Xuất dữ liệu thành công!');
+            document.getElementById('Xuatfile').style.display = 'none';
+        };
+        document.getElementById('confirm-no').onclick = () => document.getElementById('Xuatfile').style.display = 'none';
+    }
+// sidebar
+document.addEventListener("DOMContentLoaded", () => {
+  const menuItems = document.querySelectorAll(".sidebar-menu a");
+  const sections = document.querySelectorAll(".admin-section");
+
+  menuItems.forEach(item => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      const target = item.dataset.section;
+
+      // Ẩn tất cả phần
+      sections.forEach(sec => sec.style.display = "none");
+
+      // Hiện phần tương ứng
+      const active = document.getElementById(`${target}-section`);
+      if (active) active.style.display = "block";
+    });
+  });
+
+  // Mặc định hiển thị Trang chủ
+  document.getElementById("home-section").style.display = "block";
+});
